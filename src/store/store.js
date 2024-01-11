375204df283cf231518fd3c7fcbb20450c62055c
Matthew Chen <mattokc35@gmail.com>
2024-01-11 00:53:39 -0600

    redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from "redux";
 import transactionReducer from "../reducers/transactionReducer";
 import contractValuesReducer from "../reducers/contractValuesReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
   transactionId: transactionReducer, // Update the key name to match reducer
