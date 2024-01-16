1a8667ff77b109f81a9d5148903e9327f43ba1d0
Matthew Chen <mattokc35@gmail.com>
2024-01-15 18:46:48 -0600

    scroll to different sections from home links

--- src/components/divider/Divider.js
+++ src/components/divider/Divider.js
@@ -3,14 +3,14 @@
 import React from "react";
 import "./Divider.css"; // CSS for styling
 
-function Divider(props) {
+const Divider = React.forwardRef((props, ref) => {
   return (
-    <div className="divider-container">
+    <div ref={ref} className="divider-container">
       <hr className="divider-line" />
       {props.title && <div className="divider-title">{props.title}</div>}
       <hr className="divider-line" />
     </div>
   );
-}
+});
 
 export default Divider;
