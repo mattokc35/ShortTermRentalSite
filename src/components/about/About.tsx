586faab3c084a9c5eeb0deed42a36379198a5ddf
Matthew Chen <mattokc35@gmail.com>
2024-03-19 22:18:06 -0500

    about section typescript

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,12 +1,12 @@
-// About.js
 import React from "react";
 import ProfilePic from "./../../assets/benanddeborah.webp";
 import "./About.css";
 import { hostName, hostBio } from "../../constants/constants";
 
-function About() {
-  // You can replace these with actual data for the host
+// Define types for props if needed
+// For this component, no props are passed, so you can skip this step
 
+const About: React.FC = () => {
   return (
     <div className="about-section">
       <div className="profile-picture">
@@ -18,13 +18,13 @@ function About() {
           alt="Host Profile"
         />
       </div>
-      <br></br>
+      <br />
       <div className="host-details">
         <h4>{hostName}</h4>
         <p>{hostBio}</p>
       </div>
     </div>
   );
-}
+};
 
 export default About;
